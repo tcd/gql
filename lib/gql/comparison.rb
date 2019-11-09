f899e300744285c8708c3e75f2a02a431056a4f7
Clay Dunston <claydunston@gmail.com>
2019-11-09 14:37:01 -0600

    Formatting

--- lib/gql/comparison.rb
+++ lib/gql/comparison.rb
@@ -1,16 +1,21 @@
 module Gql
   # Basic implementation of `#==` and `#eql?` for comparing two class instances.
   module Comparison
+
     # Compare two class instances.
     # @param other [Object]
     # @return [Boolean]
     def ==(other)
       return other.class == self.class && other.state == self.state
     end
+
     alias eql? ==
-    # The state of a class instance, used for comparison by {==}.
+
+    # An array with all of an instance variable's values; used for comparison by {==}.
+    # @return [Array]
     def state
-      return self.instance_variables.map { |variable| self.instance_variable_get variable }
+      return self.instance_variables.map { |variable| self.instance_variable_get(variable) }
     end
+
   end
 end
