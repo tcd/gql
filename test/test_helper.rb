7be57f7659ccd75358c2abf41035fda144d58057
Clay Dunston <claydunston@gmail.com>
2019-11-09 13:52:19 -0600

    Update tooling & config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,11 +24,22 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "gql"
 
 require "minitest/autorun"
-require "pry"
+require "minitest/focus"
+require "minitest/reporters"
+Minitest::Reporters.use!([
+  Minitest::Reporters::DefaultReporter.new(color: true),
+  # Minitest::Reporters::SpecReporter.new,
+])
 
+module MiniTest::Assertions
+  # `assert_equal` with a default message.
+  def assert_equal_and_print(want, have)
+    assert_equal(want, have, "\n#{'=' * 80}\nexpected:\n#{want}\nactual:\n#{have}\n#{'=' * 80}\n")
+  end
+end
+
+# class TestCase < Minitest::Test
 # Return path to a file used in tests.
-#
-# Only works when tests are run from the project root.
 #
 # @param path [String]
 # @return [String]
@@ -38,10 +49,8 @@ end
 
 # Return JSON data from a file used for testing.
 #
-# Only works when tests are run from the project root.
-#
 # @param path [String]
 # @return [Hash<Symbol>]
 def json_file_fixture(path)
-  return Gql.parse_json(JSON.parse(file_fixture(path)))
+  return JSON.parse(File.read(file_fixture(path)), symbolize_names: true)
 end
