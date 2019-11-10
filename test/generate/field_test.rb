aa5c2a6000293cf2d7f2884f718f4d6e33221b1d
Clay Dunston <claydunston@gmail.com>
2019-11-09 18:54:06 -0600

    Remove json strings from tests, use `json_file_fixture` instead

--- test/generate/field_test.rb
+++ test/generate/field_test.rb
@@ -3,247 +3,65 @@ require "test_helper"
 class GenerateFieldTest < Minitest::Test
 
   def test_without_description
-    json = <<~JSON
-      {
-        "name": "name",
-        "description": null,
-        "args": [],
-        "type": {
-          "kind": "SCALAR",
-          "name": "String",
-          "ofType": null
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
     want = "name: String"
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/name.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_with_description
-    skip()
-    json = <<~JSON
-      {
-        "name": "name",
-        "description": "Handle of the app.",
-        "args": [],
-        "type": {
-          "kind": "SCALAR",
-          "name": "String",
-          "ofType": null
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
+    want = <<~GQL.strip()
       """Handle of the app."""
       handle: String
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/handle.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_required
-    json = <<~JSON
-      {
-        "name": "apiKey",
-        "description": "A unique application API identifier.",
-        "args": [],
-        "type": {
-          "kind": "NON_NULL",
-          "name": null,
-          "ofType": {
-            "kind": "SCALAR",
-            "name": "String",
-            "ofType": null
-          }
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
+    want = <<~GQL.strip()
       """A unique application API identifier."""
       apiKey: String!
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/api_key.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_required_list_with_required_members
-    json = <<~JSON
-      {
-        "name": "features",
-        "description": "List of app features.",
-        "args": [],
-        "type": {
-          "kind": "NON_NULL",
-          "name": null,
-          "ofType": {
-            "kind": "LIST",
-            "name": null,
-            "ofType": {
-              "kind": "NON_NULL",
-              "name": null,
-              "ofType": {
-                "kind": "SCALAR",
-                "name": "String",
-                "ofType": null
-              }
-            }
-          }
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
+    want = <<~GQL.strip()
       """List of app features."""
       features: [String!]!
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/features.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_optional_list_with_required_members
-    json = <<~JSON
-      {
-        "name": "field",
-        "description": "Path to the input field which caused the error.",
-        "args": [],
-        "type": {
-          "kind": "LIST",
-          "name": null,
-          "ofType": {
-            "kind": "NON_NULL",
-            "name": null,
-            "ofType": {
-              "kind": "SCALAR",
-              "name": "String",
-              "ofType": null
-            }
-          }
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
+    want = <<~GQL.strip()
       """Path to the input field which caused the error."""
       field: [String!]
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/field.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_required_list_with_optional_members
-    json = <<~JSON
-      {
-        "name": "not_real",
-        "description": "This is just to cover a possible edge case.",
-        "args": [],
-        "type": {
-          "kind": "NON_NULL",
-          "name": null,
-          "ofType": {
-            "kind": "LIST",
-            "name": null,
-            "ofType": {
-              "kind": "SCALAR",
-              "name": "String",
-              "ofType": null
-            }
-          }
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
+    want = <<~GQL.strip()
       """This is just to cover a possible edge case."""
       not_real: [String]!
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/not_real.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
 
   def test_field_with_arguments
-    skip()
-    json = <<~JSON
-      {
-        "name": "metafield",
-        "description": "The metafield associated with the resource.",
-        "args": [
-          {
-            "name": "namespace",
-            "description": "Container for a set of metafields (maximum of 20 characters).",
-            "type": {
-              "kind": "NON_NULL",
-              "name": null,
-              "ofType": {
-                "kind": "SCALAR",
-                "name": "String",
-                "ofType": null
-              }
-            },
-            "defaultValue": null
-          },
-          {
-            "name": "key",
-            "description": "Identifier for the metafield (maximum of 30 characters).",
-            "type": {
-              "kind": "NON_NULL",
-              "name": null,
-              "ofType": {
-                "kind": "SCALAR",
-                "name": "String",
-                "ofType": null
-              }
-            },
-            "defaultValue": null
-          }
-        ],
-        "type": {
-          "kind": "OBJECT",
-          "name": "Metafield",
-          "ofType": null
-        },
-        "isDeprecated": false,
-        "deprecationReason": null,
-        "accessRestricted": false,
-        "accessRestrictedReason": null,
-        "requiredAccess": null
-      }
-    JSON
-    want = <<~GQL.strip
-      """This is just to cover a possible edge case."""
+    want = <<~GQL.strip()
+      """The metafield associated with the resource."""
       metafield(
         "Container for a set of metafields (maximum of 20 characters)."
         namespace: String!,
@@ -251,7 +69,7 @@ class GenerateFieldTest < Minitest::Test
         key: String!
       ): Metafield
     GQL
-    field = Gql::Parse.field(JSON.parse(json, symbolize_names: true))
+    field = Gql::Parse.field(json_file_fixture("introspection-json/field/metafield.json"))
     have = Gql::Generate.field(field)
     assert_equal(want, have)
   end
