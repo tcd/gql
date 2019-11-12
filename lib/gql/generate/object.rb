51e3c899ea3108248c48d22847230511c5cc1875
Clay Dunston <claydunston@gmail.com>
2019-11-12 12:16:39 -0600

    Fix `implements` sep for `Object` (`&`, not `|`)

--- lib/gql/generate/object.rb
+++ lib/gql/generate/object.rb
@@ -4,7 +4,7 @@ module Gql
     # @return [String]
     def self.object(object)
       description = self.description(object.description)
-      interfaces  = object.interfaces.length == 0 ? "" : " implements #{object.interfaces.join(' | ')}"
+      interfaces  = object.interfaces.length == 0 ? "" : " implements #{object.interfaces.join(' & ')}"
 
       start  = description + "type " + object.name + interfaces + " {\n"
       middle = object.fields.map { |f| self.field(f) }.join(",\n")
